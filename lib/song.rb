da3d15fe19a91b9fba362c84631729d6eafaec7a
Evan Jeffries <ejeffries14@gmail.com>
2019-11-29 10:51:33 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.genre_count
+    hash = {}
+    @@genres.each do |key|
+      if hash[key]
+        hash[key] += 1
+      else hash[key] = 1
+      end
+    end
+    hash
+  end
+end
